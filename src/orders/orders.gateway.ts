339471286b1c320a81333d7cd1f5e7ac7362ae3c
Mattias Duarte <duartemattias3@gmail.com>
2025-02-28 14:33:20 -0400

    agregando localhost a wobsocket

--- src/orders/orders.gateway.ts
+++ src/orders/orders.gateway.ts
@@ -1,19 +1,27 @@
 import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway({ namespace: "/orders" })
+@WebSocketGateway({ namespace: "/orders", cors: { origin: 'http://localhost:5173', } })
 export class OrdersGateway {
-  constructor(private readonly ordersService: OrdersService) {}
+  constructor(private readonly ordersService: OrdersService) { }
 
   @WebSocketServer()
   server: Server;
 
+  // Cuando un cliente se conecta, puedes mandar órdenes directamente
+  // o dejar que el cliente emita "getOrders" y responder con "ordersList"
+  async handleConnection(client: Socket) {
+    console.log('Cliente conectado:', client.id);
+    const orders = await this.ordersService.findAll()
+    this.server.emit('refreshOrders', { orders })
+  }
+
   @SubscribeMessage('createOrder')
-  async create(@MessageBody() createOrderDto: CreateOrderDto){
+  async create(@MessageBody() createOrderDto: CreateOrderDto) {
     await this.ordersService.create(createOrderDto)
     const orders = await this.ordersService.findAll()
-    this.server.emit('refreshOrders', {orders})
+    this.server.emit('refreshOrders', { orders })
   }
 }
